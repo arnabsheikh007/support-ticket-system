8a3f3f43a386415e47e1d9d15c791d53f859e147
arnabsheikh007 <arnab.sheikh007@gmail.com>
2025-05-02 04:57:55 +0600

    Added Auth system

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,46 @@
 <?php
-
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\AdminTicketController;
+use App\Http\Controllers\SupportEngineerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    if (Auth::check()) {
+        $user = Auth::user();
+        if ($user->role === 'admin') {
+            return redirect()->route('admin.tickets.index');
+        } elseif ($user->role === 'support_engineer') {
+            return redirect()->route('support.tickets.index');
+        } else {
+            return redirect()->route('tickets.index');
+        }
+    } else {
+        return redirect()->route('login');
+    }
+});
+
+// Breeze Authentication Routes
+require __DIR__.'/auth.php';
+
+// Application Routes
+Route::middleware('auth')->group(function () {
+    Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
+    Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
+    Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
+    Route::get('/tickets/{ticket}', [TicketController::class, 'show'])->name('tickets.show');
+    Route::post('/tickets/{ticket}/comments', [TicketController::class, 'storeComment'])->name('tickets.comments.store');
+
+    Route::prefix('admin')->group(function () {
+        Route::get('/tickets', [AdminTicketController::class, 'index'])->name('admin.tickets.index');
+        Route::get('/tickets/{ticket}', [AdminTicketController::class, 'show'])->name('admin.tickets.show');
+        Route::put('/tickets/{ticket}', [AdminTicketController::class, 'update'])->name('admin.tickets.update');
+        Route::put('/tickets/{ticket}/assign', [AdminTicketController::class, 'assign'])->name('admin.tickets.assign');
+    });
+
+    Route::prefix('support')->group(function () {
+        Route::get('/tickets', [SupportEngineerController::class, 'index'])->name('support.tickets.index');
+        Route::get('/tickets/{ticket}', [SupportEngineerController::class, 'show'])->name('support.tickets.show');
+        Route::put('/tickets/{ticket}', [SupportEngineerController::class, 'update'])->name('support.tickets.update');
+        Route::put('/tickets/{ticket}/claim', [SupportEngineerController::class, 'claim'])->name('support.tickets.claim');
+    });
 });
